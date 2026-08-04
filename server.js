909fefcf0222ec1c90750a7f61114806110d8580
Celtic Glyph <59318308+Celtic-Glyph@users.noreply.github.com>
2026-08-04 17:32:11 +0100

    Refactor server.js for improved structure and routing

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 const axios = require('axios');
 const path = require('path');
 
+const app = express();
+
 // Route for the main homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
 
-const app = express();
+
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
